1c33501a37f1d71e2ce5f89e931f6e12e54d41ca
NewbieInside <shabanov.konstantin@yahoo.com>
2020-06-22 16:42:12 +0300

    header component and styling

--- src/App.js
+++ src/App.js
@@ -8,22 +8,21 @@ import StreamList from './components/StreamList/StreamList';
 import StreamShow from './components/StreamShow/StreamShow';
 import Header from './components/Header/Header';
 
- class App extends Component {
+class App extends Component {
   render() {
     return (
       <Fragment>
-        <Header />
         <Router>
-          <Route path="/" exact component={StreamList} />
-          <Route path="/streams/new" exact component={StreamCreate} />
-          <Route path="/streams/edit" exact component={StreamEdit} />
-          <Route path="/streams/delete" exact component={StreamDelete} />
-          <Route path="/streams/show" exact component={StreamShow} />
+            <Header />
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" exact component={StreamCreate} />
+            <Route path="/streams/edit" exact component={StreamEdit} />
+            <Route path="/streams/delete" exact component={StreamDelete} />
+            <Route path="/streams/show" exact component={StreamShow} />
         </Router>
       </Fragment>
-    )
+    );
   }
 }
 
-export default App
-
+export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import styles from './Header.module.css';
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header Here</h1>
+    <div className={styles.navBar}>
+      <Link className={styles.linkStyle} to="/">Streamy</Link>
+      <div>
+        <Link className={styles.linkStyle}  to="/">All Streams</Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
