e4ca85f20c3a51a55b94eb66634b0bf47ac48f68
NewbieInside <shabanov.konstantin@yahoo.com>
2020-06-22 16:08:35 +0300

    components setup and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import StreamCreate from './components/StreamCreate/StreamCreate';
+import StreamDelete from './components/StreamDelete/StreamDelete';
+import StreamEdit from './components/StreamEdit/StreamEdit';
+import StreamList from './components/StreamList/StreamList';
+import StreamShow from './components/StreamShow/StreamShow';
+import Header from './components/Header/Header';
 
  class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello React</h1>
-      </div>
+      <Fragment>
+        <Header />
+        <Router>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
+        </Router>
+      </Fragment>
     )
   }
 }
